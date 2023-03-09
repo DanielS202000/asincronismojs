77e8188d44eb591e2ee09cab4dd840bb343ad2a4
Sammuel <danielsamm62@gmail.com>
2023-03-09 02:02:59 -0600

    Callback hell

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,6 @@
-const XMLHttpRequest = require('xmhlttprequest').XMLHttpRequest;
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1';
+
 function fetchData(urlApi, callback) {
     let xhttp = new XMLHttpRequest();
 
@@ -21,7 +22,7 @@ fetchData(`${API}/products`, function(error1, data1){
     if (error1) return console.error(error1);
     fetchData(`${API}/products/${data1[0].id}`, function(error2, data2){
         if(error2) return console.error(error2);
-        fetchData(`${API}/categoria/${data2?.category?.id}`, function(error3, data3){
+        fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3){
             if (error3) return console.error(error3);
             console.log(data1[0]);
             console.log(data2.tittle);

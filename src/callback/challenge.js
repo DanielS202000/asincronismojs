fc346f486b3ed2f267c14e133c790ada6bd90e21
Sammuel <danielsamm62@gmail.com>
2023-03-09 01:14:38 -0600

    Fetch data

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('xmhlttprequest');
+const XMLHttpRequest = require('xmhlttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1';
 function fetchData(urlApi, callback) {
     let xhttp = new XMLHttpRequest();
@@ -8,11 +8,24 @@ function fetchData(urlApi, callback) {
         if (xhttp.readyState === 4) {
             if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
+            }else {
+                const error = new Error('Error' + urlApi);
+                return callback(error, null);
             }
-        } else {
-            const error = new Error('Error' + urlApi);
-            return callback(error, null);
-        }
+        } 
     }
     xhttp.send();
 }
+
+fetchData(`${API}/products`, function(error1, data1){
+    if (error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, function(error2, data2){
+        if(error2) return console.error(error2);
+        fetchData(`${API}/categoria/${data2?.category?.id}`, function(error3, data3){
+            if (error3) return console.error(error3);
+            console.log(data1[0]);
+            console.log(data2.tittle);
+            console.log(data3.name);
+        });
+    });
+});
